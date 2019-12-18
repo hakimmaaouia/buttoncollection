b1077a376338f0089b1424f7afa2830c0108cff1
hakimmaaouia <hakimmaaouia@gmail.com>
2019-12-18 18:44:29 +0100

    Updates

---
+++ precache-manifest.8bbb153ae1bfc4c379211ccbc162ac86.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1cc92610e5e6cf5e64592e951ea5d66",
+    "url": "/ButtonCollection/index.html"
+  },
+  {
+    "revision": "51cfc841411d02d02842",
+    "url": "/ButtonCollection/static/css/main.e0fae47d.chunk.css"
+  },
+  {
+    "revision": "ba1968f5cb276494cf78",
+    "url": "/ButtonCollection/static/js/2.a1972bf8.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/ButtonCollection/static/js/2.a1972bf8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "51cfc841411d02d02842",
+    "url": "/ButtonCollection/static/js/main.56a571e9.chunk.js"
+  },
+  {
+    "revision": "41cf3af657753a38c850",
+    "url": "/ButtonCollection/static/js/runtime-main.477c91f7.js"
+  }
+]);
