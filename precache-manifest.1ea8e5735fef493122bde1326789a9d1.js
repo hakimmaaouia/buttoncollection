8a8f60ea5c11063d463b7572a69784486c406161
hakimmaaouia <hakimmaaouia@gmail.com>
2019-12-18 18:56:38 +0100

    Updates

---
+++ precache-manifest.1ea8e5735fef493122bde1326789a9d1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa90d26432854a6b1298956a75cd3256",
+    "url": "/buttoncollection/index.html"
+  },
+  {
+    "revision": "6fcb087fe7e4a99f64b5",
+    "url": "/buttoncollection/static/css/main.e0fae47d.chunk.css"
+  },
+  {
+    "revision": "ba1968f5cb276494cf78",
+    "url": "/buttoncollection/static/js/2.a1972bf8.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/buttoncollection/static/js/2.a1972bf8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6fcb087fe7e4a99f64b5",
+    "url": "/buttoncollection/static/js/main.b4041145.chunk.js"
+  },
+  {
+    "revision": "3fccf71d9823bb54a5f4",
+    "url": "/buttoncollection/static/js/runtime-main.0c51c289.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/ButtonCollection/precache-manifest.8bbb153ae1bfc4c379211ccbc162ac86.js"
+  "/buttoncollection/precache-manifest.1ea8e5735fef493122bde1326789a9d1.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ButtonCollection/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/buttoncollection/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
